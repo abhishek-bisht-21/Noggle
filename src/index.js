c951ff5e3cdbd03ed3ccdde76fd2e84f3d6772c7
Abhishek <abhishek.bisht54@yahoo.com>
2022-03-30 13:20:50 +0530

    Dark mode implemented

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App'
+import './global.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<Router>
+		<App />
+	</Router>
 
-
-reportWebVitals();
+	, document.getElementById('root'));
